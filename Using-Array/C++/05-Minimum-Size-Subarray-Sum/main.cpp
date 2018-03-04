b53552321eb509bfa5bfd1e38d09b81a4f133a81
wsh <lin_ws@foxmail.com>
2018-03-04 20:19:56 +0800

    209 add slide window

--- Using-Array/C++/05-Minimum-Size-Subarray-Sum/main.cpp
+++ Using-Array/C++/05-Minimum-Size-Subarray-Sum/main.cpp
@@ -55,6 +55,33 @@ public:
             
         return res;
     }
+    
+    // 滑动窗口
+    // 时间复杂度: O(n)
+    // 空间复杂度: O(1)
+    int minSubArrayLenI(int s, vector<int>& nums) {
+        
+        assert(s > 0);
+        
+        int l = 0; r = -1; // nums[l...r]为滑动窗口
+        int sum = 0;
+        int res = nums.size() + 1;
+        
+        while (l < nums.sizes()) { //窗口的左边界在数组范围内，则循环继续
+            
+            if (r + 1 < nums.size() && sum < s)
+                sum += nums[++r];
+            else // r已经到头 或者 sum >= s
+                sum -= nums[l++];
+                
+            if (sum >= s)
+                res = min(res, r - l + 1);
+        }
+        
+        if (res == nums.size() + 1)
+            return 0;
+        return res;   
+    }
 };
 
 int main()
