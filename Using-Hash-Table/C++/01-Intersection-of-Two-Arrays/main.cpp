c67e3a8e902b30d5463ab882f9ba15f53f8bc19c
wsh <lin_ws@foxmail.com>
2018-03-08 21:59:23 +0800

    leetcode 349. set simple

--- Using-Hash-Table/C++/01-Intersection-of-Two-Arrays/main.cpp
+++ Using-Hash-Table/C++/01-Intersection-of-Two-Arrays/main.cpp
@@ -5,9 +5,9 @@
 using namespace std;
 
 // 349. Intersection of Two Arrays
-// 针对 STL set 的使用
 class Solution {
 public:
+    // 针对 STL set 的使用
     // 时间复杂度: O(nlogn)
     // 空间度咋度: O(n)
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
@@ -27,6 +27,18 @@ public:
 
         return resultVector;
     }
+    
+    vector<int> intersectionI(vector<int>& nums1, vector<int>& nums2) {
+        
+        set<int> record(nums1.begin(), nums1.end());
+
+        set<int> resultSet;
+        for (int i = 0; i < nums2.size(); i++)
+            if (record.find(nums2[i]) != record.end())
+                resultSet.insert(nums2[i]);
+
+        return vector<int>(resultSet.begin(), resultSet.end());
+    }
 };
 
 int main()
@@ -37,7 +49,7 @@ int main()
     int nums2[] = {2, 2};
     vector<int> vec2(nums2, nums2 + sizeof(nums2)/sizeof(int));
 
-    vector<int> res = Solution().intersection(vec1, vec2);
+    vector<int> res = Solution().intersectionI(vec1, vec2);
     for (int i = 0; i < res.size(); i++)
         cout << res[i] << " ";
     cout << endl;
